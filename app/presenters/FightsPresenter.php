b4c9c8d97746705a3c37cee015459e44843c44eb
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-06 18:13:14 +0200

    #6 Update na FightsPresenter.

--- app/presenters/FightsPresenter.php
+++ app/presenters/FightsPresenter.php
@@ -3,151 +3,150 @@
 namespace App\Presenters;
 
 use App\FormHelper;
-use Nette\Database\Table\Selection;
 use Nette\Database\Table\ActiveRow;
 use Nette\Application\UI\Form;
 use Nette\Application\BadRequestException;
 
-class FightsPresenter extends BasePresenter
-{
-	/** @var ActiveRow */
-	private $fightRow;
+class FightsPresenter extends BasePresenter {
 
-	/** @var string */
-	private $error = "Match not found!";
+    /** @var ActiveRow */
+    private $fightRow;
 
-	public function actionAll() {
-	}
+    /** @var ActioveRow */
+    private $roundRow;
 
-	public function renderAll()
-	{
-		$this->template->fights = $this->fightsRepository->findAll()->order('time');
-	}
+    /** @var string */
+    private $error = "Match not found!";
 
-	protected function actionCreate() {
-		$this->userIsLogged();
-	}
+    public function actionAll() {
+        
+    }
 
-	public function renderCreate() {
-		$this->getComponent( 'addFightForm' );
-	}
+    public function renderAll() {
+        $this->template->fights = $this->fightsRepository->findAll()->order('time');
+    }
 
-	public function actionEdit( $id )
-	{
-		$this->userIsLogged();
-		$this->fightRow = $this->fightsRepository->findById( $id );
-	}
+    protected function actionCreate() {
+        $this->userIsLogged();
+    }
 
-	public function renderEdit( $id ) {
-		if( !$this->fightRow )	
-			throw new BadRequestException( $this->error );
+    public function renderCreate() {
+        $this->getComponent('addFightForm');
+    }
 
-		$this->getComponent('editFightForm')->setDefaults( $this->fightRow );
-	} 
+    public function actionEdit($id) {
+        $this->userIsLogged();
+        $this->fightRow = $this->fightsRepository->findById($id);
+    }
 
-	public function actionDelete( $id )
-	{
-		$this->userIsLogged();
-		$this->fightRow = $this->fightsRepository->findById( $id );
-	}
+    public function renderEdit($id) {
+        if (!$this->fightRow)
+            throw new BadRequestException($this->error);
 
-	public function renderDelete( $id ) {
-		if( !$this->fightRow ) {
-				throw new BadRequestException( $this->error );
-		}
-		$this->template->fight =  $this->fightRow;
-	}
+        $this->getComponent('editFightForm')->setDefaults($this->fightRow);
+    }
 
-	protected function createComponentAddFightForm() {
-		$form = new Form;
+    public function actionDelete($id) {
+        $this->userIsLogged();
+        $this->fightRow = $this->fightsRepository->findById($id);
+    }
 
-		$teams = $this->teamsRepository->getTeams();
+    public function renderDelete($id) {
+        if (!$this->fightRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->fight = $this->fightRow;
+    }
 
-		$form->addSelect( 'team1_id' , 'Tím 1', $teams )
-			 ->setRequired("Názov tímu 1 je povinné pole");
+    protected function createComponentAddFightForm() {
+        $form = new Form;
 
-		$form->addSelect( 'team2_id' , 'Tím 2', $teams )
-			 ->setRequired("Názov tímu 2 je povinné pole");
+        $teams = $this->teamsRepository->getTeams();
 
-		$form->addText( 'score1' , 'Skóre 1' )
-			 ->setType( 'number' )
-			 ->setRequired("Počet bodov je povinné pole");
+        $form->addSelect('team1_id', 'Tím 1', $teams)
+                ->setRequired("Názov tímu 1 je povinné pole");
 
-		$form->addText( 'score2' , 'Skóre 2' )
-			 ->setType( 'number' )
-			 ->setRequired("Počet bodov je povinné pole");
+        $form->addSelect('team2_id', 'Tím 2', $teams)
+                ->setRequired("Názov tímu 2 je povinné pole");
 
-		$form->addText( 'time', 'Dátum' )
-			 ->setAttribute( 'value', date('Y-m-d') );
+        $form->addText('score1', 'Skóre 1')
+                ->setType('number')
+                ->setRequired("Počet bodov je povinné pole");
 
-		$form->addSubmit( 'save' , 'Uložiť' );
+        $form->addText('score2', 'Skóre 2')
+                ->setType('number')
+                ->setRequired("Počet bodov je povinné pole");
 
-		$form->onSuccess[] = $this->submittedAddFightForm;
-		FormHelper::setBootstrapFormRenderer( $form );
-		return $form;
-	}
+        $form->addText('time', 'Dátum')
+                ->setAttribute('value', date('Y-m-d'));
 
-	protected function createComponentEditFightForm() {
-		$form = new Form;
+        $form->addSubmit('save', 'Uložiť');
 
-		$teams = $this->teamsRepository->getTeams();
+        $form->onSuccess[] = $this->submittedAddFightForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
 
-		$form->addSelect( 'team1_id', 'Tím 1', $teams )
-			 ->setRequired("Názov tímu 1 je povinné pole");
+    protected function createComponentEditFightForm() {
+        $form = new Form;
 
-		$form->addSelect( 'team2_id', 'Tím 2', $teams)
-			 ->setRequired("Názov tímu je povinné pole");
+        $teams = $this->teamsRepository->getTeams();
 
-		$form->addText( 'score1' , 'Skóre 1' )
-			 ->setType( 'number' )
-			 ->setRequired("Počet bodov je povinné pole");
+        $form->addSelect('team1_id', 'Tím 1', $teams)
+                ->setRequired("Názov tímu 1 je povinné pole");
 
-		$form->addText( 'score2', 'Skóre 2' )
-			 ->setType( 'number' )
-			 ->setRequired("Počet bodov je povinné pole");
+        $form->addSelect('team2_id', 'Tím 2', $teams)
+                ->setRequired("Názov tímu je povinné pole");
 
-		$form->addText( 'time', 'Dátum' );
+        $form->addText('score1', 'Skóre 1')
+                ->setType('number')
+                ->setRequired("Počet bodov je povinné pole");
 
-		$form->addSubmit( 'save', 'Uložiť' );
-		$form->onSuccess[] = $this->submittedEditForm;
-		FormHelper::setBootstrapFormRenderer( $form );
-		return $form;
-	}
+        $form->addText('score2', 'Skóre 2')
+                ->setType('number')
+                ->setRequired("Počet bodov je povinné pole");
 
-	public function submittedAddFightForm( $form ) {
-		$values = $form->getValues();		
-		
-		if( $values->team1_id == $values->team2_id ) {
-			$form->addError( 'Zvoľ dva rozdielne tímy.' );
-			return false;
-		}
+        $form->addText('time', 'Dátum');
 
-		$this->fightsRepository->insert( $values );
-		$this->redirect('all');
-	}
+        $form->addSubmit('save', 'Uložiť');
+        $form->onSuccess[] = $this->submittedEditForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
 
-	public function submittedEditForm( $form ){
-		$values = $form->getValues();
+    public function submittedAddFightForm($form) {
+        $values = $form->getValues();
 
-		if( $values->team1_id == $values->team2_id ) {
-			$form->addError( 'Zvoľ dva rozdielne tímy.' );
-			return false;
-		}
+        if ($values->team1_id == $values->team2_id) {
+            $form->addError('Zvoľ dva rozdielne tímy.');
+            return false;
+        }
 
-		$this->fightRow->update( $values );
-		$this->redirect('all');
-	}
+        $this->fightsRepository->insert($values);
+        $this->redirect('all');
+    }
 
-	public function submittedDeleteForm()
-	{
-		$this->userIsLogged();
-		$this->fightRow->delete();
-		$this->flashMessage( 'Zápas odstránený.','success' );
-		$this->redirect('all');
-	}
+    public function submittedEditForm($form) {
+        $values = $form->getValues();
 
-	public function formCancelled() 
-	{
-		$this->redirect('all');
-	}
+        if ($values->team1_id == $values->team2_id) {
+            $form->addError('Zvoľ dva rozdielne tímy.');
+            return false;
+        }
+
+        $this->fightRow->update($values);
+        $this->redirect('all');
+    }
+
+    public function submittedDeleteForm() {
+        $this->userIsLogged();
+        $this->fightRow->delete();
+        $this->flashMessage('Zápas odstránený.', 'success');
+        $this->redirect('all');
+    }
+
+    public function formCancelled() {
+        $this->redirect('all');
+    }
+
 }

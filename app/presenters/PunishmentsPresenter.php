52b847228d7eb665f09aaa7264430940cb5606b4
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-10 23:59:21 +0200

    #10 Úprava jednotlivých trestov.

--- app/presenters/PunishmentsPresenter.php
+++ app/presenters/PunishmentsPresenter.php
@@ -2,7 +2,14 @@
 
 namespace App\Presenters;
 
+use App\FormHelper;
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+
 class PunishmentsPresenter extends BasePresenter {
+
+    /** @var ActiveRow */
+    private $punishmentRow;
 
     public function actionAll() {
         
@@ -12,4 +19,44 @@ class PunishmentsPresenter extends BasePresenter {
         $this->template->punishments = $this->punishmentsRepository->findAll();
     }
 
+    public function actionEdit($id) {
+        $this->userIsLogged();
+        $this->punishmentRow = $this->punishmentsRepository->findById($id);
+    }
+
+    public function renderEdit($id) {
+        $this->getComponent('editPunishmentForm')->setDefaults($this->punishmentRow);
+    }
+
+    public function actionDelete($id) {
+        
+    }
+
+    public function renderDelete($id) {
+        
+    }
+
+    protected function createComponentEditPunishmentForm() {
+        $form = new Form;
+        $form->addText('text', 'Trest');
+        $form->addText('round', 'Kolá');
+        $form->addSubmit('save', 'Uložiť');
+        $form->onSuccess[] = $this->submittedEditPunishmentForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
+
+    public function submittedEditPunishmentForm(Form $form) {
+        $this->userIsLogged();
+        $values = $form->getValues();
+        $this->punishmentRow->update($values);
+        $this->redirect('all');
+    }
+    
+    public function submittedDeleteForm() {
+    }
+    
+    public function formCancelled() {
+        $this->redirect('all');
+    }
 }

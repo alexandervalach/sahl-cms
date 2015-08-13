fbde9f92beb08d191239ebe81ce3573cd10cae7c
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-13 21:59:37 +0200

    Rozdelenie tabuliek na hlavnú časť a play off.

--- app/presenters/TablesPresenter.php
+++ app/presenters/TablesPresenter.php
@@ -7,117 +7,110 @@ use Nette\Application\BadRequestException;
 use Nette\Application\UI\Form;
 use Nette\Database\Table\ActiveRow;
 
-class TablesPresenter extends BasePresenter 
-{
-	/** @var ActiveRow */
-	private $tableRow;
+class TablesPresenter extends BasePresenter {
 
-	/** @var string */
-	private $error = "Row not found!";
+    /** @var ActiveRow */
+    private $tableRow;
 
-	public function actionAll() {
-	}
+    /** @var string */
+    private $error = "Row not found!";
 
-	public function renderAll() {
-		$this->template->tables = $this->tablesRepository->findAll()->order('points DESC');;
-	}
+    public function actionAll() {
+        
+    }
 
-	public function actionCreate() {
-		$this->userIsLogged();
-	}
+    public function renderAll() {
+        $this->template->basic = $this->tablesRepository->findByValue('type',0)->order('points DESC');
+        $this->template->playoff = $this->tablesRepository->findByValue('type',1)->order('points DESC');
+    }
 
-	public function renderCreate() {
-		$this->getComponent('addTableRowForm');
-	}
+    public function actionCreate() {
+        $this->userIsLogged();
+    }
 
-	public function actionEdit( $id ) {
-		$this->userIsLogged();
-		$this->tableRow = $this->tablesRepository->findById( $id );
-	}
+    public function renderCreate() {
+        $this->getComponent('addTableRowForm');
+    }
 
-	public function renderEdit( $id ) {
-		if(!$this->tableRow)	throw new BadRequestException( $this->error );
-		$this->getComponent('editTableRowForm')->setDefaults( $this->tableRow );
-	}
+    public function actionEdit($id) {
+        $this->userIsLogged();
+        $this->tableRow = $this->tablesRepository->findById($id);
+    }
 
-	public function actionDelete( $id ) {
-		$this->userIsLogged();
-		$this->tableRow = $this->tablesRepository->findById( $id );
-	}
+    public function renderEdit($id) {
+        if (!$this->tableRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->team = $this->tableRow;
+        $this->getComponent('editTableRowForm')->setDefaults($this->tableRow);
+    }
 
-	public function renderDelete( $id ) {
-		if ( !$this->tableRow ) throw new BadRequestException ( $this->error ); 
-		$this->template->table = $this->tableRow;
-	}
+    public function actionDelete($id) {
+        $this->userIsLogged();
+        $this->tableRow = $this->tablesRepository->findById($id);
+    }
 
-	protected function createComponentAddTableRowForm() {
-		$form = new Form;
+    public function renderDelete($id) {
+        if (!$this->tableRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->table = $this->tableRow;
+        $this->getComponent('deleteForm');
+    }
 
-		$teams = $this->teamsRepository->getTeams();
+    protected function createComponentAddTableRowForm() {
+        $form = new Form;
+        $teams = $this->teamsRepository->getTeams();
+        $form->addSelect('team_id', 'Mužstvo', $teams);
+        $form->addSubmit('save', 'Uložiť');
 
-		$form->addSelect('team_id','Mužstvo', $teams);
+        $form->onSuccess[] = $this->submittedAddTableRowForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
 
-		$form->addSubmit('save','Uložiť');
+    protected function createComponentEditTableRowForm() {
+        $form = new Form;
 
-		$form->onSuccess[] = $this->submittedAddTableRowForm;
-		FormHelper::setBootstrapFormRenderer( $form );
-		return $form;
-	}
+        $teams = $this->teamsRepository->getTeams();
 
-	protected function createComponentEditTableRowForm() {
-		$form = new Form;
+        $form->addText('win', 'Výhry');
+        $form->addText('tram', 'Remízy');
+        $form->addText('lost', 'Prehry');
+        $form->addText('score1', 'Skóre 1');
+        $form->addText('score2', 'Skóre 2');
+        $form->addText('points', 'Body'); 
+        $form->addCheckbox('type', ' Play off');
+        $form->addSubmit('save', 'Uložiť');
 
-		$teams = $this->teamsRepository->getTeams();
+        $form->onSuccess[] = $this->submittedEditTableRowForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
 
-		$form->addHidden('team_id');
+    public function submittedAddTableRowForm(Form $form) {
+        $this->userIsLogged();
+        $values = $form->getValues();
+        $this->tablesRepository->insert($values);
+        $this->redirect('all');
+    }
 
-		$form->addText('win','Výhry')
-			 ->setType('number');
+    public function submittedEditTableRowForm(Form $form) {
+        $this->userIsLogged();
+        $values = $form->getValues();
+        $this->tableRow->update($values);
+        $this->redirect('all');
+    }
 
-		$form->addText('tram','Remízy')
-			 ->setType('number');
+    public function submittedDeleteForm() {
+        $this->userIsLogged();
+        $this->tableRow->delete();
+        $this->flashMessage('Záznam zmazaný!', 'success');
+        $this->redirect('all');
+    }
 
-		$form->addText('lost','Prehry')
-			 ->setType('number');
+    public function formCancelled() {
+        $this->redirect('all');
+    }
 
-		$form->addText('score1','Skóre 1')
-			 ->setType('number');
-
-		$form->addText('score2','Skóre 2')
-			 ->setType('number');
-
-		$form->addText('points','Body')
-			 ->setType('number');
-
-		$form->addSubmit('save','Uložiť');
-
-		$form->onSuccess[] = $this->submittedEditTableRowForm;
-		FormHelper::setBootstrapFormRenderer( $form );
-		return $form;
-	}
-
-	public function submittedAddTableRowForm( Form $form ) {
-		$this->userIsLogged();
-		$values = $form->getValues();
-		$this->tablesRepository->insert($values);
-		$this->redirect('all');
-	}
-
-	public function submittedEditTableRowForm( Form $form ) {
-		$this->userIsLogged();
-		$values = $form->getValues();	
-		$this->tableRow->update($values);
-		$this->redirect('all');
-	}
-
-	public function submittedDeleteForm() {
-		$this->userIsLogged();
-		$this->tableRow->delete();
-		$this->flashMessage('Záznam zmazaný!','success');
-		$this->redirect('all');
-	}
-
-	public function formCancelled() {
-		$this->redirect('all');
-	}
 }

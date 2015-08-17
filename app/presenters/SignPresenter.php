9377f178ce5c726a54ef2011c43306269fadfea3
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-17 10:51:13 +0200

    Zmena prihlasovacieho formulára.

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -2,55 +2,49 @@
 
 namespace App\Presenters;
 
+use App\FormHelper;
 use Nette;
 
 /**
  * Sign in/out presenters.
  */
-class SignPresenter extends BasePresenter
-{
+class SignPresenter extends BasePresenter {
 
-	/**
-	 * Sign-in form factory.
-	 * @return Nette\Application\UI\Form
-	 */
-	protected function createComponentSignInForm()
-	{
-		$form = new Nette\Application\UI\Form;
-		$form->addText('username', 'Užívateľské meno:')
-			 ->setRequired('Zadaj, prosím, užívateľské meno.');
+    /**
+     * Sign-in form factory.
+     * @return Nette\Application\UI\Form
+     */
+    protected function createComponentSignInForm() {
+        $form = new Nette\Application\UI\Form;
+        $form->addText('username', 'Užívateľské meno:')
+                ->setRequired('Zadaj, prosím, užívateľské meno.');
 
-		$form->addPassword('password', 'Heslo:')
-			 ->setRequired('Zadaj, prosím, heslo.');
+        $form->addPassword('password', 'Heslo:')
+                ->setRequired('Zadaj, prosím, heslo.');
 
-		$form->addSubmit('send', 'Prihlásiť');
+        $form->addSubmit('send', 'Prihlásiť');
 
-		// call method signInFormSucceeded() on success
-		$form->onSuccess[] = $this->submittedSignInForm;
+        // call method signInFormSucceeded() on success
+        $form->onSuccess[] = $this->submittedSignInForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
 
-		return $form;
-	}
+    public function submittedSignInForm($form) {
+        $values = $form->values;
 
+        try {
+            $this->getUser()->login($values->username, $values->password);
+            $this->redirect('Homepage:');
+        } catch (Nette\Security\AuthenticationException $e) {
+            $form->addError('Nesprávne meno alebo heslo.');
+        }
+    }
 
-	public function submittedSignInForm($form)
-	{
-		$values = $form->values;
-
-		try {
-			$this->getUser()->login($values->username, $values->password);
-			$this->redirect('Homepage:');
-
-		} catch (Nette\Security\AuthenticationException $e) {
-			$form->addError('Nesprávne meno alebo heslo.');
-		}
-	}
-
-
-	public function actionOut()
-	{
-		$this->getUser()->logout();
-		$this->flashMessage('Boli ste odhlásený.','success');
-		$this->redirect('Homepage:');
-	}
+    public function actionOut() {
+        $this->getUser()->logout();
+        $this->flashMessage('Boli ste odhlásený.', 'success');
+        $this->redirect('Homepage:');
+    }
 
 }

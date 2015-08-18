5a8d29eaf171bb50a8f836b6c39f7a8a2e1c0df4
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-18 15:38:25 +0200

    #23 Link na funkčnú odpoveď vo fóre.

--- app/presenters/ReplyPresenter.php
+++ app/presenters/ReplyPresenter.php
@@ -2,9 +2,15 @@
 
 namespace App\Presenters;
 
+use App\FormHelper;
+use Nette\Application\UI\Form;
 use Nette\Database\Table\ActiveRow;
+use Nette\Application\BadRequestException;
 
 class ReplyPresenter extends BasePresenter {
+
+    /** @var ActiveRow */
+    private $forumRow;
 
     /** @var ActiveRow */
     private $replyRow;
@@ -13,11 +19,16 @@ class ReplyPresenter extends BasePresenter {
     private $error = "Reply not found!";
 
     public function actionAdd($id) {
-        
+        $this->userIsLogged();
+        $this->forumRow = $this->forumRepository->findById($id);
     }
 
     public function renderAdd($id) {
-        
+        if (!$this->forumRow) {
+            throw new BadRequestException("Message not found!");
+        }
+        $this->template->forum = $this->forumRow;
+        $this->getComponent('addForm');
     }
 
     public function actionDelete($id) {
@@ -27,7 +38,27 @@ class ReplyPresenter extends BasePresenter {
     public function renderDelete($id) {
         
     }
-    
+
+    protected function createComponentAddForm() {
+        $form = new Form;
+        $form->addText('author', 'Meno')
+                ->setRequired("Meno je povinné pole")
+                ->addRule(Form::MAX_LENGTH, "Maximálna dĺžka mena je 50 znakov", 50);
+        $form->addTextArea('text', 'Text')
+                ->setAttribute('class', 'form-jqte');
+        $form->addSubmit('add', 'Pridaj');
+        $form->onSuccess[] = $this->submittedAddForm;
+        FormHelper::setBootstrapFormRenderer($form);
+        return $form;
+    }
+
+    public function submittedAddForm(Form $form) {
+        $values = $form->getValues();
+        $values['forum_id'] = $this->forumRow->id;
+        $this->replyRepository->insert($values);
+        $this->redirect('Forum:all');
+    }
+
     public function formCancelled() {
         $this->redirect('Forum:all');
     }

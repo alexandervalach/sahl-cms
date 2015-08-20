871ca99caeed3b98c734f58853e9956997774d93
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-20 14:55:10 +0200

    #25 Prerobenie funkcií v repozitári

--- app/model/FightsRepository.php
+++ app/model/FightsRepository.php
@@ -6,39 +6,39 @@ use Nette\Database\Table\ActiveRow;
 
 class FightsRepository extends Repository {
 
-    public function getTeamForFight(ActiveRow $row, $key) {
-        return $row->ref('teams', $key);
+    public function getForFight(ActiveRow $row, $key, $table = 'teams') {
+        return $row->ref($table, $key);
     }
 
-    public function getPlayersForTeam(ActiveRow $row, $key) {
-        $players = $this->getTeamForFight($row, $key)->related('players');
+    public function getPlayersForFight(ActiveRow $row, $key, $table = 'teams') {
+        return $this->getForFight($row, $key, $table)->related('players');
+    }
+
+    public function getPlayersForTeam(ActiveRow $row, $key, $table = 'teams') {
+        $players = $this->getPlayetsForFight($row, $table, $key);
         $list = array();
         foreach ($players as $player) {
             $list[$player->id] = $player->lname;
         }
         return $list;
     }
-    
-    public function getPlayersForFight(ActiveRow $row, $key) {
-       return $this->getTeamForFight($row, $key)->related('players');
-    }
 
-    public function getPlayersForSelect(ActiveRow $row, $key1, $key2) {
-        $teamOnePlayers = $this->getPlayersForFight($row, $key1);
-        $teamTwoPlayers = $this->getPlayersForFight($row, $key2);
-     
+    public function getPlayersForSelect(ActiveRow $row, $key1, $key2, $table = 'teams') {
+        $teamOnePlayers = $this->getPlayersForFight($row, $key1, $table);
+        $teamTwoPlayers = $this->getPlayersForFight($row, $key2, $table);
+
         foreach ($teamOnePlayers as $player) {
             $teamOneIdList[] = $player->id;
             $teamOneNameList[] = $player->lname;
         }
-        
+
         foreach ($teamTwoPlayers as $player) {
             $teamTwoIdList[] = $player->id;
             $teamTwoNameList[] = $player->lname;
         }
-        
-        $idList = array_merge( $teamOneIdList, $teamTwoIdList);
-        $nameList = array_merge( $teamOneNameList, $teamTwoNameList );
+
+        $idList = array_merge($teamOneIdList, $teamTwoIdList);
+        $nameList = array_merge($teamOneNameList, $teamTwoNameList);
         $list = array_combine($idList, $nameList);
         return $list;
     }

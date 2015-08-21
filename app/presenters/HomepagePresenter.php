b94af4e26907796e6f261ec305147362130e90d1
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-21 12:11:45 +0200

    #30 Pridanie komponenty Visual Paginator do Homepage.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,25 +3,36 @@
 namespace App\Presenters;
 
 use Nette;
+use IPub\VisualPaginator\Components as VisualPaginator;
 
 /**
  * Homepage presenter.
  */
 class HomepagePresenter extends BasePresenter {
 
-    /** @var Nette\Database\Selection */
-    private $post;
-
     public function renderDefault() {
         $limit = 4;
-        $posts = $this->postsRepository->findAll();
+        $postSelection = $this->postsRepository->findAll()->order('id DESC');
 
-        $latests = $posts->order('created_at ASC')->limit($limit);
+        $latests = $this->postsRepository->findAll()->order('id DESC')->limit($limit);
 
-        $this->template->posts = $posts;
+        $visualPaginator = $this->getComponent('visualPaginator');
+        $paginator = $visualPaginator->getPaginator();
+        $paginator->itemsPerPage = 5;
+        $paginator->itemCount = $postSelection->count();
+        $postSelection->limit($paginator->itemsPerPage, $paginator->offset);
+
+        $this->template->posts = $postSelection;
         $this->template->latests = $latests;
         $this->template->default = "/images/sahl.jpg";
         $this->template->imgFolder = $this->imgFolder;
+    }
+
+    protected function createComponentVisualPaginator() {
+        $control = new VisualPaginator\Control;
+        $control->setTemplateFile('bootstrap.latte');
+        $control->disableAjax();
+        return $control;
     }
 
 }

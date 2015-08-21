b73fd6e2314f16d8e08b5e8ba18e2a81dc8e3086
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-21 14:43:39 +0200

    #31 Portrait obrázky sa znázorňujú na konci v plnej veľkosti.

--- www/js/gallery.js
+++ www/js/gallery.js
@@ -1,18 +1,45 @@
 $(function () {
-    
     var options = {
         image: $('.animate'),
         fadeSpeed: 500
     }
 
-    var img = options.image;
-    img.css({opacity: 0.6});
+    screenWidth = $('body').innerWidth();
+    console.log(screenWidth);
 
-    img.mouseenter(function () {
-        $(this).not(':animated').animate({opacity: 1}, options.fadeSpeed);
-    });
+    var portrait = [];
+    var figure;
+    var section;
 
-    img.mouseleave(function () {
-        $(this).animate({opacity: 0.6}, options.fadeSpeed);
-    });
+    if (screenWidth >= 974) {
+        var img = options.image;
+        img.each(function () {
+            var width = $(this).width();
+            var height = $(this).height();
+            if (height > width) {
+                // $(this).height(0.75 * width);
+                figure = $(this).parent().parent();
+                section = figure.parent();
+                portrait.push( figure.html() );
+                figure.detach();
+                console.log(figure);
+            }
+        });
+        
+        console.log(section);
+
+        $.each(portrait, function ( index, value ) {
+            section.append( value );
+            console.log( index + ": " + value );
+        });
+
+        img.css({opacity: 0.6});
+
+        img.mouseenter(function () {
+            $(this).not(':animated').animate({opacity: 1}, options.fadeSpeed);
+        });
+        img.mouseleave(function () {
+            $(this).animate({opacity: 0.6}, options.fadeSpeed);
+        });
+    }
 })

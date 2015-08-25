ddf23c1f9b4cbaa41d43b5973b5b825050840c52
Jon Snow <Jon Snow@Lenovo-PC>
2015-08-25 14:08:09 +0200

    Vylepšenie Homepage switchu.

--- www/js/steamSlider.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.img-gallery').click(function() {
-		var src = $(this).attr('src');
-
-		$('#current').attr('src', src);
-		//console.log(src);
-	});
-});
--- www/js/switch.js
+++ www/js/switch.js
@@ -1,27 +1,33 @@
 // jQuery news slider on Hompage:default
 
-$(function() {
+$(function () {
+    var slider = $('#slider');
+    var para = slider.find('p');
+    var img = slider.find('img');
+    var paraHeight = para.height();
+    
+    var options = {
+        speed: 100,
+        plusHeight: 10
+    };
 
-	var slider = $('#slider');
-	var para = slider.find('p');
-	var img = slider.find('img');
-	var paraHeight = para.height();
-	
-	para.bind('mouseover', function() {
-		var src = $(this).attr('data-src');
+    para.bind('mouseover', function () {
+        var src = $(this).attr('data-src');
 
-		img.not(":animated").animate({ opacity: 0 }, 250, function() {
-			img.attr('src', src);
-			img.animate({ opacity: 1}, 250);
-		});
-	});
+        if (img.attr('src') !== src) {
+            img.not(":animated").animate({opacity: 0}, options.speed, function () {
+                img.attr('src', src);
+                img.animate({opacity: 1}, options.speed);
+            });
+        }
+    });
 
-	para.bind('mouseenter', function() {
-		var item = $(this).not(":animated");
-		item.animate({height : paraHeight + 10}, 250)
-		
-	}).bind('mouseleave', function() {
-		$(this).animate({height : paraHeight }, 250);	
-	})
+    para.bind('mouseenter', function () {
+        var item = $(this).not(":animated");
+        item.animate({height: paraHeight + options.plusHeight}, options.speed);
+
+    }).bind('mouseleave', function () {
+        $(this).animate({height: paraHeight}, options.speed);
+    });
 
 });

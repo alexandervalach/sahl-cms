38edfedd0b63f6dde7e7822f1143a2e36fad0534
jonsnow <jonsnow@jonsnow-Lenovo-B590>
2016-01-21 14:47:34 +0100

    Pridanie typu hráča do databázy.

--- app/presenters/PlayerTypesPresenter.php
+++ app/presenters/PlayerTypesPresenter.php
@@ -2,6 +2,9 @@
 
 namespace App\Presenters;
 
+use App\FormHelper;
+use Nette\Application\UI\Form;
+
 class PlayerTypesPresenter extends BasePresenter {
     public function actionAll() {
         $this->userIsLogged();
@@ -9,5 +12,31 @@ class PlayerTypesPresenter extends BasePresenter {
     
     public function renderAll() {
         $this->template->types = $this->playerTypesRepository->findAll();
+    }
+    
+    public function actionAdd() {
+        $this->userIsLogged();
+    }
+    
+    public function renderAdd() {
+        $this->getComponent('addForm');
+    }
+    
+    protected function createComponentAddForm() {
+        $form = new Form;
+        $form->addText('type', 'Typ hráča')
+                ->addRule(Form::MAX_LENGTH, 'Názov môže mať len 50 znakov.', 50)
+                ->setRequired();
+        $form->addSubmit('save', 'Uložiť');
+        
+        FormHelper::setBootstrapFormRenderer($form);
+        $form->onSuccess[] = $this->submittedAddForm;
+        return $form;
+    }
+    
+    public function submittedAddForm(Form $form) {
+        $values = $form->getValues();
+        $this->playerTypesRepository->insert($values);
+        $this->redirect('all#primary');
     }
 }

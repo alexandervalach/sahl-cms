af4f913298629d557abf9b0b1a4ade7ba3fd676b
jonsnow <jonsnow@jonsnow-Lenovo-B590>
2016-01-26 15:32:46 +0100

    Odstránenie nadbytočných funkcií.

--- app/model/FightsRepository.php
+++ app/model/FightsRepository.php
@@ -10,37 +10,4 @@ class FightsRepository extends Repository {
         return $row->ref($table, $key);
     }
 
-    public function getPlayersForFight(ActiveRow $row, $key, $table = 'teams') {
-        return $this->getForFight($row, $key, $table)->related('players');
-    }
-
-    public function getPlayersForTeam(ActiveRow $row, $key, $table = 'teams') {
-        $players = $this->getPlayetsForFight($row, $table, $key);
-        $list = array();
-        foreach ($players as $player) {
-            $list[$player->id] = $player->lname;
-        }
-        return $list;
-    }
-
-    public function getPlayersForSelect(ActiveRow $row, $key1, $key2, $table = 'teams') {
-        $teamOnePlayers = $this->getPlayersForFight($row, $key1, $table);
-        $teamTwoPlayers = $this->getPlayersForFight($row, $key2, $table);
-
-        foreach ($teamOnePlayers as $player) {
-            $teamOneIdList[] = $player->id;
-            $teamOneNameList[] = $player->lname . " - " . $player->num;
-        }
-
-        foreach ($teamTwoPlayers as $player) {
-            $teamTwoIdList[] = $player->id;
-            $teamTwoNameList[] = $player->lname . " - " . $player->num;
-        }
-
-        $idList = array_merge($teamOneIdList, $teamTwoIdList);
-        $nameList = array_merge($teamOneNameList, $teamTwoNameList);
-        $list = array_combine($idList, $nameList);
-        return $list;
-    }
-
 }

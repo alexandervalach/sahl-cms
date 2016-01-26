6c2f125fb5dad24359731c67b6a107b80a4320d5
jonsnow <jonsnow@jonsnow-Lenovo-B590>
2016-01-26 15:33:23 +0100

    Pridanie novej akcie do repozitára.

--- app/model/PlayersRepository.php
+++ app/model/PlayersRepository.php
@@ -2,17 +2,33 @@
 
 namespace App\Model;
 
-class PlayersRepository extends Repository{
+class PlayersRepository extends Repository {
+
     public function getPlayers() {
         $players = $this->findAll();
         $list = array();
-        
-        foreach($players as $player) {
-           $list[$player->id] = $player->lname; 
+
+        foreach ($players as $player) {
+            $list[$player->id] = $player->lname;
         }
-        
+
         return $list;
     }
-}
 
-?>
+    public function getPlayersByValue($key, $value) {
+        $players = $this->findByValue($key, $value)->order('lname ASC');
+        if ($players == null) {
+            return null;
+        }
+
+        $list = array();
+
+        foreach ($players as $player) {
+            if ($player->num != 0) {
+                $list[$player->id] = $player->lname . ' - ' . $player->num;
+            }
+        }
+        return $list;
+    }
+
+}
--- app/model/TeamsRepository.php
+++ app/model/TeamsRepository.php
@@ -2,22 +2,20 @@
 
 namespace App\Model;
 
-class TeamsRepository extends Repository{
-	
-	/**
-	 * Loop trought all teams and store them in array. 
-	 * Accessible index $team->id. 
-	 * Value is $team->name.
-	 * @return array
-	 */
-	public function getTeams() {
-		$teamSelection = $this->findAll()->order('name ASC');
-		$teams = array();
-		foreach( $teamSelection as $team ){
-			$teams[$team->id] = $team->name;
-		}	
-		return $teams;
-	}
-}
+class TeamsRepository extends Repository {
 
-?>
+    /**
+     * Loop trought all teams and store them in array. 
+     * Accessible index $team->id. 
+     * Value is $team->name.
+     * @return array
+     */
+    public function getTeams() {
+        $teamSelection = $this->findAll()->order('name ASC');
+        $teams = array();
+        foreach ($teamSelection as $team) {
+            $teams[$team->id] = $team->name;
+        }
+        return $teams;
+    }
+}

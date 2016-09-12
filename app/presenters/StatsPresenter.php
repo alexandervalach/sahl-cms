cd0ede39f738cdc38c13a9ded629677069c55b8c
Jon Snow <alexander.valach@gmail.com>
2016-09-12 11:05:06 +0200

    Pridanie archívu do štatistík

--- app/presenters/StatsPresenter.php
+++ app/presenters/StatsPresenter.php
@@ -9,7 +9,16 @@ class StatsPresenter extends BasePresenter {
     }
 
     public function renderAll() {
-        $this->template->stats = $this->playersRepository->findAll()->where('lname != ?', ' ')->order('goals DESC, lname DESC');
+        $this->template->stats = $this->playersRepository->findByValue('archive_id', null)->where('lname != ?', ' ')->order('goals DESC, lname DESC');
+    }
+
+    public function actionArchive($id) {
+
+    }
+
+    public function renderArchive($id) {
+    	$this->template->stats = $this->playersRepository->findByValue('archive_id', $id)->where('lname != ?', ' ')->order('goals DESC, lname DESC');
+    	$this->template->archive = $this->archiveRepository->findById($id);
     }
 
 }

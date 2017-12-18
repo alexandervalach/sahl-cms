8647bb354b36eec576a3d6a56bd15f5c56a748ba
Alexander Valach <alexander.valach@gmail.com>
2017-12-18 09:48:52 +0100

    Zmeny v CSS a JS

--- www/js/main.js
+++ www/js/main.js
@@ -7,16 +7,15 @@ $(function () {
 
     show_scroll_btn(body, scroll_btn);
     scrol_to_top(body, scroll_btn);
-});
 
-$(document).ajaxComplete( function() {
-    var main = $('#snippet--main').height();
-    var flash = $('#snippet--flash').height();
-    var new_height = main + flash + 50;
-    
-    $('#ajax').animate({
-        height: new_height
-    }, 200)
+    $(document).ajaxComplete( function() {
+        var main = $('#snippet--main').height();
+        var flash = $('#snippet--flash').height();
+        var new_height = main + flash;
+
+        $('#ajax').animate({ height: new_height}, 'fast');
+    });
+
 });
 
 function scrol_to_top(body, scroll_btn) {

1fc4354ea09b414481d1b85d11cafc4ce7553e46
Alexander Valach <alexander.valach@gmail.com>
2026-08-07 15:45:44 +0200

    Updated bootstrap.php to reflect NETTE_DEBUG variable and set to false by default

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,15 +4,21 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('147.175.177.65');
+$debug = filter_var(
+    getenv('NETTE_DEBUG') ?: 'false',
+    FILTER_VALIDATE_BOOLEAN
+);
+
+$configurator->setDebugMode($debug);
 $configurator->enableDebugger(__DIR__ . '/../log');
+
 error_reporting(~E_USER_DEPRECATED);
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->createRobotLoader()
-        ->addDirectory(__DIR__)
-        ->register();
+    ->addDirectory(__DIR__)
+    ->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
